ddd025613a6a612b67c31fd997f368fc0ef738d2
knxghtly <knxtly1596@gmail.com>
2025-09-05 16:37:59 +0900

    @RequestParam

--- src/main/java/com/practice/basic1/HomeController.java
+++ src/main/java/com/practice/basic1/HomeController.java
@@ -2,22 +2,20 @@ package com.practice.basic1;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
 public class HomeController {
-    private int count;
 
-    public HomeController(){
-        count = -1;
-    }
-
-    @GetMapping("/home/increase")
+    @GetMapping("/home/plus")
     @ResponseBody
-    // @ResponseBody: 아래 메서드 실행 후 리턴값을 응답으로 해줘
-    public int showIncrease(){
-        ++count;
-        return count;
+    // @ResponseBody: 아래 메서드 실행 후 리턴값을 body에 반응해줘
+    public int showPlus(
+            // @RequestParam: int a는 쿼리스트링에서 a값을 얻은 후 정수화한 값이다
+            @RequestParam(required = false, defaultValue = "1") int a,
+            @RequestParam(defaultValue = "0") int b){
+        return a * b;
     }
 }

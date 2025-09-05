e599281828d7502a0289a139ee81fea44a25861f
knxghtly <knxtly1596@gmail.com>
2025-09-05 15:49:06 +0900

    increase 예시

--- src/main/java/com/practice/basic1/HomeController.java
+++ src/main/java/com/practice/basic1/HomeController.java
@@ -4,12 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+
 @Controller
 public class HomeController {
-    @GetMapping("/home/main")
+    private int count;
+
+    public HomeController(){
+        count = -1;
+    }
+
+    @GetMapping("/home/increase")
     @ResponseBody
     // @ResponseBody: 아래 메서드 실행 후 리턴값을 응답으로 해줘
-    public String showHome(){
-        return "Hello";
+    public int showIncrease(){
+        ++count;
+        return count;
     }
 }
